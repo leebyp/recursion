ced4774e141f082d371d7a348199546dbdf774ad
Benjamin Lee <leebyp@gmail.com>
2014-02-24 19:51:45 +0800

    Created function in src/stringifyJSON.js but no passes in SpecRunner.html

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,38 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  function addToResult(target){
+		if (Array.isArray(target)){
+			var individualResult = '[';
+			var individualArray = [];
+			for (var i=0; i<target.length; i++){
+				if (target[i] !== undefined && typeof target[i] !== 'function'){
+					individualArray.push(addToResult(target[i]));
+				}
+			}
+			individualResult += individualArray.join(',')
+			individualResult += ']';
+			return individualResult;
+		}
+		else if (typeof target === 'object'){
+			var individualResult = '{';
+			var individualArray = [];
+			for (var x in target){
+				if (target[x] !== undefined && typeof target[x] !== 'function'){
+					individualArray.push('"'+ x + '"' + ':' + addToResult(target[x]));
+				}
+			}
+			individualResult += individualArray.join(',')
+			individualResult += '}';
+			return individualResult;		
+		}
+		else if (typeof target === 'string'){
+			return '"' + target + '"';
+		}
+		else {
+			return ''+target;
+		}
+	}
+
+	return addToResult(obj);
 };
