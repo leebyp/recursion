6b14121dfa3c903b87f2226c042618b1d03e2bf8
Benjamin Lee <leebyp@gmail.com>
2014-02-25 02:15:46 +0800

    Created function in src/getElementsByClassName.js but still error in SpecRunner

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,31 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+
+	function addToResult(node){
+		if (node === document.body){
+			var resultArray = [];
+			for (var i=1; i<node.childNodes.length; i++){
+				addToResult(node.childNodes[i]);
+			}
+			return resultArray;
+		}
+		else {
+			if (node.classList !== undefined){
+				for (var i=0; i<node.classList.length; i++){
+					if (node.classList[i] === className){
+						resultArray.push(node);
+					}
+				}
+			}
+			if (node.childNodes.length !== 0){
+				for (var i=0; i<node.childNodes.length; i++){
+					addToResult(node.childNodes[i]);
+				}
+			}
+		}
+	}
+
+	return addToResult(document.body);
+
 };
