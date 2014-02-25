5eee20140d737395ed3a11aee11d973d74a07ba8
Benjamin Lee <leebyp@gmail.com>
2014-02-25 15:18:56 +0800

    Planned general structure of building parseJSON.js function.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -5,3 +5,27 @@
 var parseJSON = function (json) {
   // your code goes here
 };
+
+
+
+
+
+
+
+
+general format:
+    go through string character by character,
+    encounter " => return string until next "
+    encounter [ => return array until next ]
+    encounter { => return object until next }
+    encounter 0-9 (including decimals) => return number until not 0-9
+    encounter true/false/null => return true/false/null if next characters fit
+
+functions needed:
+    nextCharacter (advance in text and check against specialchars)
+    skipWhitespace (not useful unless in string)
+    parseString
+    parseArray
+    parseObject
+    parseNumber
+    parseWord
